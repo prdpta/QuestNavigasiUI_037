54a5179a0cc061b630f74e18432c4ba2a4d910d0
prdpta <pradiptaputramei01@gmail.com>
2025-11-07 21:15:38 +0700

    membuat tombol "Kembali" ke halaman formulir

--- app/src/main/java/com/example/questnavigasiui_037/view/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasiui_037/view/Navigasi.kt
@@ -43,3 +43,8 @@ fun DataApp(
         }
     }
 }
+private fun cancelAndBackToFormulir(
+    navController: NavHostController
+) {
+    navController.popBackStack(route = Navigasi.Formulirku.name, inclusive = false)
+}
